ed93333d2556bdea443384415b5b2197479d2549
morgan thorne <morgangthorne@outlook.com>
2025-02-24 09:30:12 -0500

    Add project files.

---
+++ Chapter4Exercise5-BodyMassIndex.cpp
@@ -0,0 +1,21 @@
+/*
+File Name: Chapter4Exercise5-BodyMassIndex.cpp
+Programmer: Morgan Thorne
+Date: February 2025
+Requirements:
+Write a prorgram that calculates and displays a person's
+body mass index (BMI). The program should display a message
+indicating whether the person has optimal weight, underweight
+or overweight.
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    ;
+}
+
+
